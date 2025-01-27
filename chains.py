9c5d564c1f9f2eed2fc0360f0a73b457c9d583df
Modi Yatri <yatri1610@gmail.com>
2025-01-27 14:56:10 +0530

    Initial commit

---
+++ chains.py
@@ -0,0 +1,55 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def news_extract(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+             ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the news article page of a website.
+            your job is to extract the content of news article and return the summarizes lenghty news articles into concise,easy-to-read summaries,keypoint form.
+            divede in catagories if possible.
+            Only return valid JSON.
+            ### VALID JSON ONLY(NO PREAMBLE):
+            """
+)
+
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res 
+
+    def write_summaries(self,json_res):
+        prompt_summaries = PromptTemplate.from_template(
+        """
+
+        ### NEWS ARTICLE CONTENT:
+        {news_article_content}
+        ### INSTRUCTION:
+        your job is to summarize the lenghty content of  above dictionary and formate into easy-to-read summaries.
+        Ensure that keypoint are clear,and the summary is written in  simple language for quick understanding.
+        Please generate a plain text summary of the following news report. Do not use any markdown syntax or special characters (like asterisks or underscores) for formatting.
+        ### SUMMARY (NO PREAMBLE):
+        """
+        )
+        chain_summaries = prompt_summaries | self.llm
+        res = chain_summaries.invoke({"news_article_content":dict(json_res)})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
---
+++ main.py
@@ -0,0 +1,87 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from utils import clean_text
+
+def makers_details():
+    st.subheader("Meet the Makers")
+    
+    makers = [
+        {"name": "Modi Yatri", "linkedin": "https://www.linkedin.com/in/yatri-modi-607825315"},
+        {"name": "Bhoi Sunilkumar", "linkedin": "https://www.linkedin.com/in/sunil-bhoi-00104a251"},
+    ]
+
+    for maker in makers:
+        st.write(f"**{maker['name']}**")
+        st.markdown(f"[LinkedIn Profile]({maker['linkedin']})")
+
+
+def get_in_touch():
+    st.markdown("**Want to get in touch?**")  
+    st.markdown("For further inquiries, feel free to email us at: [bhois0648@gmail.com](mailto:bhois0648@gmail.com)")
+
+
+def feedback_form():
+    st.title("Feedback Form")
+    st.markdown("We value your feedback! Please share your thoughts to help us improve.")
+    
+    st.markdown("---")
+
+    feedback = st.text_area("**Please leave your feedback here:**", height=150)
+
+    if st.button("Submit Feedback"):
+        if feedback:
+            with open("feedback.txt", "a") as f:
+                f.write(feedback + "\n")
+            st.success("Thank you for your feedback! 😊")
+        else:
+            st.error("Please enter your feedback before submitting.")
+
+
+def create_streamlit_app(llm, clean_text):
+    st.title("📰 News Article Summarizer")
+    st.markdown("""
+        Enter the URL of a news article, and this tool will generate a concise summary.
+        """)
+    st.markdown("---")
+   
+   
+    
+    url_input = st.text_input("Enter a URL:", value="https://indianexpress.com", help="Paste a valid news article URL here.")
+    submit_button = st.button("🚀 Summarize Now")
+
+    if submit_button and url_input:
+        with st.spinner("Hold tight! Great things take time... 🌟"): 
+            try:
+                loader = WebBaseLoader([url_input])
+                data = clean_text(loader.load().pop().page_content)
+                
+                json_res = llm.news_extract(data)
+                samary = llm.write_summaries(json_res)
+                
+                st.success("Summary generated!")
+                st.code(samary, language='markdown')
+                
+            except Exception as e:
+                st.error(f"An Error Occurred: {e}")
+            
+
+if __name__ == "__main__":
+    chain = Chain()
+    st.set_page_config(layout="wide", page_title=" News Article Summarizer", page_icon="📰")
+    create_streamlit_app(chain, clean_text)
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        if st.button("Feedback"):
+            feedback_form() 
+
+    with col2:
+        if st.button("Meet the Makers"):
+            makers_details()
+    
+    with col3:
+        if st.button("Get in Touch"):
+            get_in_touch()
